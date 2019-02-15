0cd31e503ade047cc2b379784ea875c83e01e4bc
Samuel Li <samuelli4521@gmail.com>
2019-02-15 09:19:00 -0800

    added basic mobile compaitbility to entire website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,23 +28,19 @@ class Contact extends Component {
   render() {
     return (
       <div id={"contact"} className={this.state.visible ? "visible" : ""}>
-        <div className={"background"}>
-          <div className={"img"}/>
-          <div className={"gradient"}/>
-        </div>
-        <div className={"text"}>
-          <h1>Contact Us</h1>
-          <p style={{marginBottom: 25}}>You can contact us at <a style={{color: "white"}} href="mailto:prospectrobotics2813@gmail.com">prospectrobotics2813@gmail.com</a> or at our social media handles.</p>
-          <a href="https://www.facebook.com/team2813/" target={"_blank"} style={{marginRight: 25}}>
-            <img src={facebook} alt="Facebook" height={100}/>
-          </a>
-          <a href="https://www.instagram.com/frc2813/" target={"_blank"}>
-            <img src={instagram} alt="instagram" height={100}/>
-          </a>
-          <a href="https://twitter.com/frc2813" target={"_blank"}>
-            <img src={twitter} alt="twitter" height={100}/>
-          </a>
-        </div>
+        <h1>Contact Us</h1>
+        <p style={{marginBottom: 25}}>You can contact us at <a style={{color: "white"}}
+                                                               href="mailto:prospectrobotics2813@gmail.com">prospectrobotics2813@gmail.com</a> or
+          at our social media handles.</p>
+        <a href="https://www.facebook.com/team2813/" target={"_blank"} style={{marginRight: 25}}>
+          <img src={facebook} alt="Facebook" height={100}/>
+        </a>
+        <a href="https://www.instagram.com/frc2813/" target={"_blank"}>
+          <img src={instagram} alt="instagram" height={100}/>
+        </a>
+        <a href="https://twitter.com/frc2813" target={"_blank"}>
+          <img src={twitter} alt="twitter" height={100}/>
+        </a>
       </div>
     )
   }
