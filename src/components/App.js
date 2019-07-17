23a98b856af5b1cd688ed06ca62f6a40e2264ed2
Samuel Li <samuelli4521@gmail.com>
2019-07-17 11:59:07 +0800

    added notification for website change

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import '../styles/App.css';
 import Landing from "../pages/Landing";
 import Scroller from "./Scroller";
 import Members from "../pages/Members";
